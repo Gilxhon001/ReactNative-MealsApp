b4786069f6c399b9c0384c42ebf0b81cf62e1db9
Gilxhon Hima <89044420+Gilxhon001@users.noreply.github.com>
2023-05-11 14:53:34 +0000

    updates

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,4 +1,35 @@
-function CategoriesScreen() {
+import {FlatList, StyleSheet} from "react-native";
+import {CATEGORIES} from '../data/dummy-data';
+import CategoryGridTitle from '../components/CategoryGridTitle'
+
+
+function CategoriesScreen({navigation}) {
+    function renderCategoryItem(itemData) {
+        function pressHandler() {
+            navigation.navigate('MealsOverview', {
+                categoryId: itemData.item.id,
+            })
+        }
+
+        return (
+            <CategoryGridTitle
+                title={itemData.item.title}
+                color={itemData.item.color}
+                onPress={pressHandler}
+            />
+        )
+    }
+
+
+    return (
+        <FlatList data={CATEGORIES}
+                  key={'#'}
+                  keyExtractor={(item) => item.id}
+                  renderItem={renderCategoryItem}
+                  numColumns={2}
+        />
+    )
 }
 
 export default CategoriesScreen;
+
---
+++ screens/MealDetailsScreen.js
@@ -0,0 +1,17 @@
+import {View, FlatList, StyleSheet, Text, Image} from "react-native";
+
+
+export default function MealDetailsScreen ({route})  {
+    const mealId = route.params.mealId;
+    return <View>
+                <Image />
+                <Text></Text>
+                <View>
+
+                </View>
+           </View>
+}
+
+const styles = StyleSheet.create({
+
+});
